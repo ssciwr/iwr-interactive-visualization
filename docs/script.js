8aa7d5415dcb068f40b20a51fc5aa33d10c24c09
Liam Keegan <liam@keegan.ch>
2022-01-19 15:41:05 +0100

    minor improvements
    - add drop-shadow
    - refactor code to reduce duplication

--- docs/script.js
+++ docs/script.js
@@ -89,58 +89,76 @@ function makeSegment(radius, startAngle, endAngle, width) {
   ].join(" ");
 }
 
+var resetGroups = function () {
+  SVG.find(".segment-item").css({ opacity: 1, filter: "grayscale(0)" });
+  SVG.find(".group-item").show();
+};
+
+var showGroups = function () {
+  SVG.find(".segment-item").css({ filter: "grayscale(80%)", opacity: "20%" });
+  this.css({ opacity: 1, filter: "grayscale(0)" });
+  var groups = this.data("groups");
+  var items = SVG.find(".group-item");
+  for (var i = 0; i < groups.length; i++) {
+    if (groups[i] == 0) {
+      items[i].hide();
+    } else {
+      items[i].show();
+    }
+  }
+};
+
+var resetHighlights = function () {
+  SVG.find(".segment-item").css({ opacity: 1, filter: "grayscale(0)" });
+};
+
+var weightHighlights = function () {
+  var weights = this.data("method_weights");
+  var items = SVG.find(".method-item");
+  items.css({ filter: "grayscale(80%)", opacity: "20%" });
+  for (var i = 0; i < weights.length; i++) {
+    if (weights[i] > 0) {
+      items[i].css({ filter: "grayscale(0%)" });
+      items[i].css({ opacity: weights[i] });
+    }
+  }
+  var weights = this.data("application_weights");
+  var items = SVG.find(".application-item");
+  items.css({ filter: "grayscale(80%)", opacity: "20%" });
+  for (var i = 0; i < weights.length; i++) {
+    if (weights[i] > 0) {
+      items[i].css({ filter: "grayscale(0%)" });
+      items[i].css({ opacity: weights[i] });
+    }
+  }
+};
+
+function addSegments(svg, names, groups, colors, radius, width, segmentClass) {
+  var delta = 360 / names.length;
+  for (var i = 0; i < names.length; i++) {
+    var group = svg.group().addClass("segment-item").addClass(segmentClass);
+    group.mouseover(showGroups);
+    group.mouseout(resetGroups);
+    group.data("text", names[i]);
+    group.data("groups", groups[i]);
+    var itemPath = group
+      .path(makeSegment(radius, i * delta, (i + 1) * delta, width))
+      .fill(colors[i])
+      .stroke("#000000")
+      .css({ filter: "drop-shadow(1px 1px 2px)" });
+    var strPath = group
+      .path(makeArc(radius - Math.ceil(width / 2), i * delta, (i + 1) * delta))
+      .fill("none")
+      .stroke("none");
+    strPath
+      .text(names[i])
+      .attr("startOffset", "50%")
+      .attr("text-anchor", "middle");
+  }
+}
+
 window.onload = function () {
   var s = SVG("#svg-menu");
-
-  var resetGroups = function () {
-    SVG.find(".method-item").css({ opacity: 1, filter: "grayscale(0)" });
-    SVG.find(".application-item").css({ opacity: 1, filter: "grayscale(0)" });
-    SVG.find(".group-item").show();
-  };
-
-  var showGroups = function () {
-    SVG.find(".method-item").css({ filter: "grayscale(80%)", opacity: "20%" });
-    SVG.find(".application-item").css({
-      filter: "grayscale(80%)",
-      opacity: "20%",
-    });
-    this.css({ opacity: 1, filter: "grayscale(0)" });
-    var groups = this.data("groups");
-    var items = SVG.find(".group-item");
-    for (var i = 0; i < groups.length; i++) {
-      if (groups[i] == 0) {
-        items[i].hide();
-      } else {
-        items[i].show();
-      }
-    }
-  };
-
-  var resetHighlights = function () {
-    SVG.find(".method-item").css({ opacity: 1, filter: "grayscale(0)" });
-    SVG.find(".application-item").css({ opacity: 1, filter: "grayscale(0)" });
-  };
-
-  var weightHighlights = function () {
-    var weights = this.data("method_weights");
-    var items = SVG.find(".method-item");
-    items.css({ filter: "grayscale(80%)", opacity: "20%" });
-    for (var i = 0; i < weights.length; i++) {
-      if (weights[i] > 0) {
-        items[i].css({ filter: "grayscale(0%)" });
-        items[i].css({ opacity: weights[i] });
-      }
-    }
-    var weights = this.data("application_weights");
-    var items = SVG.find(".application-item");
-    items.css({ filter: "grayscale(80%)", opacity: "20%" });
-    for (var i = 0; i < weights.length; i++) {
-      if (weights[i] > 0) {
-        items[i].css({ filter: "grayscale(0%)" });
-        items[i].css({ opacity: weights[i] });
-      }
-    }
-  };
 
   // groups
   var h = 150 / group_names.length;
@@ -156,56 +174,30 @@ window.onload = function () {
       .cx(200)
       .cy(145 + h * i)
       .fill("#cccccc")
-      .stroke("#000000");
+      .stroke("#000000")
+      .css({ filter: "drop-shadow(1px 1px 2px)" });
     group
       .text(group_names[i])
       .cx(200)
       .cy(145 + h * i);
   }
 
-  // methods
-  var r = 157;
-  var delta = 360 / method_names.length;
-  for (var i = 0; i < method_names.length; i++) {
-    var group = s.group().addClass("method-item");
-    group.mouseover(showGroups);
-    group.mouseout(resetGroups);
-    group.data("text", method_names[i]);
-    group.data("groups", method_groups[i]);
-    var itemPath = group
-      .path(makeSegment(r, i * delta, (i + 1) * delta, 13))
-      .fill(method_colors[i])
-      .stroke("#000000");
-    var strPath = group
-      .path(makeArc(r - 7, i * delta, (i + 1) * delta))
-      .fill("none")
-      .stroke("none");
-    strPath
-      .text(method_names[i])
-      .attr("startOffset", "50%")
-      .attr("text-anchor", "middle");
-  }
-
-  // applications
-  var r = 183;
-  var delta = 360 / application_names.length;
-  for (var i = 0; i < application_names.length; i++) {
-    var group = s.group().addClass("application-item");
-    group.data("text", application_names[i]);
-    group.data("groups", application_groups[i]);
-    group.mouseover(showGroups);
-    group.mouseout(resetGroups);
-    var itemPath = group
-      .path(makeSegment(r, i * delta, (i + 1) * delta, 13))
-      .fill(application_colors[i])
-      .stroke("#000000");
-    var strPath = group
-      .path(makeArc(r - 8, i * delta, (i + 1) * delta))
-      .fill("none")
-      .stroke("none");
-    strPath
-      .text(application_names[i])
-      .attr("startOffset", "50%")
-      .attr("text-anchor", "middle");
-  }
+  addSegments(
+    s,
+    method_names,
+    method_groups,
+    method_colors,
+    157,
+    13,
+    "method-item"
+  );
+  addSegments(
+    s,
+    application_names,
+    application_groups,
+    application_colors,
+    183,
+    13,
+    "application-item"
+  );
 };
