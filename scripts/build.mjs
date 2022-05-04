d68faab539eab7961c2163b450828b7116335393
Liam Keegan <liam@keegan.ch>
2022-05-04 10:56:32 +0200

    read data as json from url on pageload
    - replace src/js/data.ts with src/data/data.json
    - re-organise data to make it easier to edit
    - json is loaded from relative url "fileadmin/iwr_vis/data.json"
    - "colour" -> "color" in JSON & code for consistency with css
    - add image_base_url
    - use image filenames from json instead of auto-constructing from prof name
    - put data.json and images in fileadmin/iwr_vis subfolder to suit existing IWR website folder structure
    - resolves #105

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,19 +3,24 @@ import shell from "shelljs";
 import ConvertTiff from "tiff-to-png";
 import glob from "glob";
 
-// copy html & css to dist folder
+// copy html, css to dist folder
 shell.mkdir("-p", "./dist");
 shell.cp("./src/html/index.html", "./dist/.");
 shell.cp("./src/css/style.css", "./dist/.");
 
-// convert tif images to png & write to dist folder
+// copy json data to dist/fileadmin/iwr_vis
+shell.mkdir("-p", "./dist/fileadmin");
+shell.mkdir("-p", "./dist/fileadmin/iwr_vis");
+shell.cp("./src/data/data.json", "./dist/fileadmin/iwr_vis/.");
+
+// convert tif images to png & write to dist/fileadmin/iwr_vis folder
 const options = {
   cwd: ".",
 };
 const converter = new ConvertTiff();
 const forFiles = (err, files) => {
   if (err) console.log(err);
-  converter.convertArray(files, "./dist");
+  converter.convertArray(files, "./dist/fileadmin/iwr_vis");
 };
 glob("**/img/*.tif", options, forFiles);
 
