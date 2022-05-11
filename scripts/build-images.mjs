e241e2f32c196f9967538d78f673e57ed947b04a
Liam Keegan <liam@keegan.ch>
2022-05-11 16:04:02 +0200

    copy any jpg/png images from src/img at build time
    - update image filename in data.json & fix umlaut in filename

--- scripts/build-images.mjs
+++ scripts/build-images.mjs
@@ -15,3 +15,8 @@ const forFiles = (err, files) => {
   converter.convertArray(files, image_dir).then(console.log);
 };
 glob("**/img/*.tif", options, forFiles);
+
+// just copy any jpg or png images to image_dir
+shell.cp("./src/img/*.jpg", image_dir);
+shell.cp("./src/img/*.jpeg", image_dir);
+shell.cp("./src/img/*.png", image_dir);
